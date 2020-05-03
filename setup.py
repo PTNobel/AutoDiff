23fdedb2900fd6cefe7c75b20d989583457f9fae
Parth Nobel <parthnobel@berkeley.edu>
2020-05-03 15:31:37 -0700

    master: Bumps version number for release two today.
    Changes committed:
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="auto_diff",
-    version="0.3.1",
+    version="0.3.3",
     author="Parth Nobel",
     author_email="parthnobel@berkeley.edu",
     description="An automatic differentiation library for Python+NumPy.",
