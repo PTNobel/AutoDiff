master: Adds test case for and fixes issue with multiplication broadcasting.
Changes committed:
	modified:   auto_diff/vecvalder_funcs_and_ufuncs.py
	modified:   tests/test_auto_diff_unit.py

auto_diff/vecvalder_funcs_and_ufuncs.py
@@ -216,6 +216,7 @@ def positive(x, /, out):
 
 
 # Tested
+# TODO: Reimplement
 @register(np.add)
 @_add_out_support
 def add(x1, x2, /, out):
@@ -231,6 +232,7 @@ def add(x1, x2, /, out):
 
 
 # Tested
+# TODO: Test broadcasting
 @register(np.subtract)
 @_add_out_support
 def subtract(x1, x2, /, out):
@@ -248,24 +250,39 @@ def subtract(x1, x2, /, out):
 
 
 # Tested
+# TODO: Reimplement
 @register(np.multiply)
 @_add_out_support
 def multiply(x1, x2, /, out):
+    broadcast_obj = np.broadcast(x1, x2)
+    idx_iterator = modded_np.ndindex(broadcast_obj.shape)
+    if out.val is None:
+        out = modded_np.empty(broadcast_obj.shape)
+ 
     if isinstance(x1, cls) and isinstance(x2, cls):
-        return cls(np.multiply(x1.val, x2.val, out=out.val),
-                   np.add(_chain_rule(x2.val, x1.der),
-                          _chain_rule(x1.val, x2.der), out=out.der))
+        for idx, (x1_elem, x2_elem) in zip(idx_iterator, broadcast_obj):
+            out.val[idx] = x1_elem.val * x2_elem.val
+            out.der[idx] = x1_elem.der * x2_elem.val + x1_elem.val * x2_elem.der
+        return cls(out.val, out.der)
+
     elif isinstance(x1, cls):
-        return cls(np.multiply(x1.val, x2, out=out.val),
-                   np.multiply(x1.der, x2, out=out.der))
+        for idx, (x1_elem, x2_elem) in zip(idx_iterator, broadcast_obj):
+            out.val[idx] = x1_elem.val * x2_elem
+            out.der[idx] = x1_elem.der * x2_elem
+        return cls(out.val, out.der)
+
     elif isinstance(x2, cls):
-        return cls(np.multiply(x1, x2.val, out=out.val),
-                   np.multiply(x1, x2.der, out=out.der))
+        for idx, (x1_elem, x2_elem) in zip(idx_iterator, broadcast_obj):
+            out.val[idx] = x1_elem * x2_elem.val
+            out.der[idx] = x1_elem * x2_elem.der
+        return cls(out.val, out.der)
+
     else:
         raise RuntimeError("This should not be occuring.")
 
 
 # Tested
+# TODO: Reimplement
 @register(np.true_divide)
 @_add_out_support
 def true_divide(x1, x2, /, out):
@@ -284,6 +301,7 @@ def true_divide(x1, x2, /, out):
         raise RuntimeError("This should not be occuring.")
 
 
+# TODO: Reimplement
 @register(np.float_power)
 @_add_out_support
 def float_power(x1, x2, /, out):
@@ -298,6 +316,7 @@ def float_power(x1, x2, /, out):
         raise RuntimeError("This should not be occuring.")
 
 
+# TODO: Reimplement
 @register(np.power)
 @_add_out_support
 def power(x1, x2, /, out):
@@ -313,6 +332,7 @@ def power(x1, x2, /, out):
 
 
 
+# TODO: Reimplement
 # Partially Tested
 @register(np.matmul)
 @_add_out_support

tests/test_auto_diff_unit.py
@@ -9,10 +9,13 @@ class AutoDiffUnitTesting(unittest.TestCase):
 
 
 class TestSingleVariableAutoDiff(AutoDiffUnitTesting):
-    def _test_helper(self, f, x, df_dx):
+    def _test_helper(self, f, x, df_dx, debug=False):
+        if debug:
+            breakpoint()
+
         input_x = x
-        f_x = f(x)
-        with auto_diff.AutoDiff(x) as x:
+        f_x = f(input_x)
+        with auto_diff.AutoDiff(input_x) as x:
             y, Jf = auto_diff.get_value_and_jacobian(f(x))
 
         self._assertAllClose(y, f_x)
@@ -31,9 +34,11 @@ class TestSingleVariableAutoDiff(AutoDiffUnitTesting):
         self._assertAllClose(y, f_x)
         self._assertAllClose(Jf, df_dx)
 
-    def _test_out(self, f, x, df_dx):
+    def _test_out(self, f, x, df_dx, debug=False):
+        if debug:
+            breakpoint()
         input_x = x
-        f_x = f(x)
+        f_x = f(input_x)
 
         with auto_diff.AutoDiff(input_x) as x:
             out_dest = np.ndarray(f_x.shape)
@@ -77,7 +82,7 @@ class TestSingleVariableAutoDiff(AutoDiffUnitTesting):
         with self.assertWarns(UserWarning, msg='abs of a near-zero number, derivative is ill-defined'):
             self._test_helper(f, x, df_dx)
             self._test_out(f, x, df_dx)
-        
+
     def test_sqrt(self):
         f = np.sqrt
         x = np.array([[2.], [4.], [9.0]])
@@ -252,7 +257,10 @@ class TestSingleVariableAutoDiff(AutoDiffUnitTesting):
 
 
 class TestMultipleVariableAutoDiff(AutoDiffUnitTesting):
-    def _test_helper(self, f, x, u, df_dx, df_du):
+    def _test_helper(self, f, x, u, df_dx, df_du, debug=False):
+        if debug:
+            breakpoint()
+
         f_xu = f(x, u)
 
         with auto_diff.AutoDiff(x, u) as (x, u):
@@ -275,6 +283,16 @@ class TestMultipleVariableAutoDiff(AutoDiffUnitTesting):
         u = np.array([[.2, 8.3, .5]]).T
 
         self._test_helper(lambda x, u: A @ x + B @ u, x, u, A, B)
+
+    def test_add_and_mulitply(self):
+        def f(x, u):
+            A = np.array([[2., -1], [-1, 2]])
+            B = np.array([[1.] , [0]])
+            return A @ x + B * u
+
+        x = np.array([[5.], [3.]])
+        u = np.array([[3.]])
+        self._test_helper(f, x, u, np.array([[2, -1], [-1, 2.]]), np.array([[1], [0.]]))
 
 
 if __name__ == '__main__':