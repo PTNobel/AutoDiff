461d57eb8de93808b4a8e1a5b8d1f24c7b27bc88
Parth Nobel <parthnobel@berkeley.edu>
2023-02-03 16:37:39 -0800

    Fixes version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="auto_diff",
-    version="0.3.5",
+    version="0.4.1",
     author="Parth Nobel",
     author_email="parthnobel@berkeley.edu",
     description="An automatic differentiation library for Python+NumPy.",
